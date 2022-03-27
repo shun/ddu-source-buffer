ed10080ec0adb6e0d6f8abd263e0766320908419
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-03-27 11:21:06 +0900

    Fix type errors

--- denops/@ddu-sources/buffer.ts
+++ denops/@ddu-sources/buffer.ts
@@ -6,7 +6,7 @@ import { Denops, fn } from "https://deno.land/x/ddu_vim@v1.1.0/deps.ts";
 import { relative } from "https://deno.land/std@0.123.0/path/mod.ts#^";
 
 type ActionData = {
-  bufnr: number;
+  bufNr: number;
   path: string;
   isCurrent: boolean;
   isAlternate: boolean;
@@ -26,13 +26,12 @@ export class Source extends BaseSource<Params> {
   gather(args: {
     denops: Denops;
   }): ReadableStream<Item<ActionData>[]> {
-    const get_actioninfo = async(bufnr_: number, path: string): ActionInfo => {
+    const get_actioninfo = async(bufnr_: number, path: string): Promise<ActionInfo> => {
       const curnr_ = await fn.bufnr(args.denops, "%");
       const altnr_ = await fn.bufnr(args.denops, "#");
-      const name_ = await fn.bufname(args.denops, bufnr_);
       const isCurrent_ = curnr_ === bufnr_ ? true : false;
       const isAlternate_ = altnr_ === bufnr_ ? true : false;
-      const isModified_ = await fn.getbufvar(args.denops, bufnr_, "&modified");
+      const isModified_ = await fn.getbufvar(args.denops, bufnr_, "&modified") as boolean;
 
       const curmarker_ = isCurrent_ ? "%" : "";
       const altmarker_ = isAlternate_ ? "#" : "";
@@ -58,7 +57,7 @@ export class Source extends BaseSource<Params> {
       const altnr_ = await fn.bufnr(args.denops, "#");
 
       let path = "";
-      path = await fn.expand(args.denops, `#${altnr_}:p`);
+      path = await fn.expand(args.denops, `#${altnr_}:p`) as string;
       if (await fn.filereadable(args.denops, path)) {
         buffers.push(await get_actioninfo(altnr_, path));
       }
@@ -76,7 +75,7 @@ export class Source extends BaseSource<Params> {
           continue;
         }
 
-        path = await fn.expand(args.denops, `#${i}:p`);
+        path = await fn.expand(args.denops, `#${i}:p`) as string;
         if (! await fn.filereadable(args.denops, path)) {
           continue;
         }
